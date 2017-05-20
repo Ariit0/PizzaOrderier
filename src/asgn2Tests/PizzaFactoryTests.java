b91ae53f44f78ab945f96520fbdeab3e0cd46986
Ari Luangamath <ari1@live.com.au>
2017-05-20 20:58:01 +1000

    Implemented PizzaFactoryTests

--- src/asgn2Tests/PizzaFactoryTests.java
+++ src/asgn2Tests/PizzaFactoryTests.java
@@ -1,6 +1,12 @@
 package asgn2Tests;
 
+import java.time.LocalTime;
 
+import org.junit.Test;
+
+import asgn2Exceptions.PizzaException;
+import asgn2Pizzas.Pizza;
+import asgn2Pizzas.PizzaFactory;
 /** 
  * A class that tests the asgn2Pizzas.PizzaFactory class.
  * 
@@ -8,5 +14,70 @@ package asgn2Tests;
  * 
  */
 public class PizzaFactoryTests {
-	// TO DO
+	Pizza margheritaPizza;
+	Pizza meatLoversPizza;
+	Pizza vegetarianPizza;
+	
+	@Test
+	public void TestPizzaFactory() throws PizzaException {
+		margheritaPizza = PizzaFactory.getPizza("PZM", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		meatLoversPizza = PizzaFactory.getPizza("PZL", 1, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		vegetarianPizza = PizzaFactory.getPizza("PZV", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+	}
+	
+	@Test (expected = PizzaException.class)
+	public void TestIncorrectPizzaCodes() throws PizzaException {
+		margheritaPizza = PizzaFactory.getPizza("ASD", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		meatLoversPizza = PizzaFactory.getPizza("RTY", 1, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		vegetarianPizza = PizzaFactory.getPizza("YUI", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+	}
+	
+	@Test (expected = PizzaException.class)
+	public void TestShortPizzaCodes() throws PizzaException {
+		margheritaPizza = PizzaFactory.getPizza("P", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		meatLoversPizza = PizzaFactory.getPizza("Z", 1, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		vegetarianPizza = PizzaFactory.getPizza("Z", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+	}
+	
+	@Test (expected = PizzaException.class)
+	public void TestLongPizzaCodes() throws PizzaException {
+		margheritaPizza = PizzaFactory.getPizza("PZMM", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		meatLoversPizza = PizzaFactory.getPizza("PZLL", 1, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		vegetarianPizza = PizzaFactory.getPizza("PZVV", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+	}
+	
+	@Test (expected = PizzaException.class)
+	public void TestEmptyPizzaCodes() throws PizzaException {
+		margheritaPizza = PizzaFactory.getPizza("", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		meatLoversPizza = PizzaFactory.getPizza("", 1, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		vegetarianPizza = PizzaFactory.getPizza("", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+	}
+	
+	@Test (expected = PizzaException.class)
+	public void TestWhiteSpaceOnlyCodes() throws PizzaException {
+		margheritaPizza = PizzaFactory.getPizza("   ", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		meatLoversPizza = PizzaFactory.getPizza("   ", 1, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		vegetarianPizza = PizzaFactory.getPizza("   ", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+	}
+	
+	@Test (expected = PizzaException.class)
+	public void TestWhiteSpacedCode() throws PizzaException {
+		margheritaPizza = PizzaFactory.getPizza("P Z M", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		meatLoversPizza = PizzaFactory.getPizza("P Z L", 1, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		vegetarianPizza = PizzaFactory.getPizza("P Z V", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+	}
+	
+	@Test (expected = PizzaException.class)
+	public void TestSpacesBeforeCode() throws PizzaException {
+		margheritaPizza = PizzaFactory.getPizza("   PZM", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		meatLoversPizza = PizzaFactory.getPizza("   PZL", 1, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		vegetarianPizza = PizzaFactory.getPizza("   PZV", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+	}
+	
+	@Test (expected = PizzaException.class)
+	public void TestSpacesAfterCode() throws PizzaException {
+		margheritaPizza = PizzaFactory.getPizza("PZM   ", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		meatLoversPizza = PizzaFactory.getPizza("PZL   ", 1, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+		vegetarianPizza = PizzaFactory.getPizza("PZV   ", 2, LocalTime.parse("19:00:00"), LocalTime.parse("19:30:00"));
+	}
 }
