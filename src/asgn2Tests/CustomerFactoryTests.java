efb0ef69c97009446fbda6de9e5815ae56fcc3cf
George <george-land@hotmail.com>
2017-05-20 17:59:58 +1000

    Added CustomerFactory tests (4)

--- src/asgn2Tests/CustomerFactoryTests.java
+++ src/asgn2Tests/CustomerFactoryTests.java
@@ -1,5 +1,11 @@
 package asgn2Tests;
 
+import org.junit.Test;
+import asgn2Customers.Customer;
+import asgn2Customers.CustomerFactory;
+import asgn2Exceptions.CustomerException;
+
+
 /**
  * A class the that tests the asgn2Customers.CustomerFactory class.
  * 
@@ -7,5 +13,26 @@ package asgn2Tests;
  *
  */
 public class CustomerFactoryTests {
-	// TO DO
+	//PUC, DNC, DVC
+	Customer customer;
+	
+	@Test
+	public void TestGetPickUpCustomer() throws CustomerException {
+		customer = CustomerFactory.getCustomer("PUC", "Bobby", "0412345678", 0, 0);
+	}
+	
+	@Test
+	public void TestGetDriverDeliveryCustomer() throws CustomerException {
+		customer = CustomerFactory.getCustomer("DVC", "Bobby", "0412345678", 2, 3);
+	}
+	
+	@Test
+	public void TestGetDroneDeliveryCustomer() throws CustomerException {
+		customer = CustomerFactory.getCustomer("DNC", "Bobby", "0412345678", 7, -5);
+	}
+	
+	@Test (expected = CustomerException.class)
+	public void TestGetInvalidCodeCustomer() throws CustomerException {
+		customer = CustomerFactory.getCustomer("DNCC", "Bobby", "0412345678", 7, -5);
+	}
 }
